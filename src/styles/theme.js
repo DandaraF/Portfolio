eed14c27c565b18eaad67344a11170a6dd85ec97
Dandara Silva <dandara-chr@hotmail.com>
2022-01-29 23:03:06 -0300

    create theme site

---
+++ src/styles/theme.js
@@ -0,0 +1,32 @@
+const Theme = {
+  palette: {
+    text: {
+      primary: "#999999)",
+      secondary: "#FFFFFF",
+    },
+    warning: "",
+    success: "",
+    background: {
+      default: "#1E1E1E",
+      primary: "#080808",
+      secondary: "#7F01C8",
+      tertiary: {
+        tertiary_1: "#1BC4F5",
+        tertiary_2: "#C90E31",
+        tertiary_3: "#09D33D",
+        tertiary_4: "#F8D61B",
+        tertiary_5: "#1E53D7",
+        tertiary_6: "#F39C14",
+        tertiary_7: "#0A5685",
+        tertiary_8: "#005841",
+        tertiary_9: "#001D35",
+      },
+      step: {
+        primary: "#",
+        secondary: "#",
+      },
+    },
+  },
+};
+
+export default Theme;
