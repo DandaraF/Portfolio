1484be878687e2fb3c175cb2ad06bf94a53a49aa
Dandara Silva <dandara-chr@hotmail.com>
2022-01-30 20:23:55 -0300

    create footer

---
+++ src/mocks/data.js
@@ -0,0 +1,16 @@
+import * as I from "./../assert/img";
+
+export const contact = [
+  {
+    index: 1,
+    img: I.github,
+    text: "Github",
+    page: "https://github.com/DandaraF?tab=repositories",
+  },
+  {
+    index: 2,
+    img: I.linkedin,
+    text: "Linkedin",
+    page: "https://www.linkedin.com/in/dandarafsilva/",
+  },
+];
